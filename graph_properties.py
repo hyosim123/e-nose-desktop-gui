90f37c2efdfde95ed708e4a6994fa778f5edfe0c
Kgangadhar <karthikg1643@gmail.com>
2019-01-09 16:25:42 -0600

    initial commit

---
+++ graph_properties.py
@@ -0,0 +1,48 @@
+
+
+
+""" Properties to apply to the graphs"""
+
+__author__ = 'Karthik Gangadhara'
+
+
+class GraphProps(object):
+    """ Bucket to hold graph properties"""
+
+    def __init__(self):
+
+        self.amp_plot = {'xlabel': "'time'",
+                         'ylabel': "'current'",
+                         'xlim': "[0, 200]",
+                         'ylim': "[-.1,1]",
+                         'title': "'Amperometry time course'",
+                         'subplots_adjust': "bottom=0.15, left=0.12"}
+        self.cv_plot = {'xlabel': "'time (sec)'",
+                        'ylabel': "u'resistance (kilo-ohm)'",
+                        'title': "'Chromo Amperometry'",
+                        'subplots_adjust': "bottom=0.15, left=0.12"}
+        self.sensor_plot = {'xlabel': "'voltage (mV)'",
+                        'ylabel': "u'current (\u00B5A)'",
+                        'title': "'Cyclic Voltammetry'",
+                        'subplots_adjust': "bottom=0.15, left=0.12"}                        
+        self.cv_canvas = {'xlabel': "voltage (mV)",
+                          'ylabel': u"current (\u00B5A)",
+                          'width': 600,
+                          'height': 300,
+                          'xlim': [-500, 500],
+                          'ylim': [-1, 1],
+                          'title': "Cyclic Voltammetry"}
+        self.sensor_canvas = {'xlabel': "voltage (mV)",
+                          'ylabel': u"current (\u00B5A)",
+                          'width': 600,
+                          'height': 300,
+                          'xlim': [-500, 500],
+                          'ylim': [-1, 1],
+                          'title': "Cyclic Voltammetry"}                          
+        self.amp_canvas = {'xlabel': "time",
+                           'ylabel': "current",
+                           'width': 600,
+                           'height': 300,
+                           'xlim': [000, 200],
+                           'ylim': [-1, 1],
+                           'title': "Amperometry time course"}
