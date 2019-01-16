e9915a526f7e033458d66d4d9d2ad4a032392b1a
Kgangadhar <karthikg1643@gmail.com>
2019-01-16 13:13:32 -0600

    updated options menu

--- option_menu.py
+++ option_menu.py
@@ -27,19 +27,19 @@ class OptionMenu(tk.Menu):
         file_menu = tk.Menu(menubar, tearoff=0)
         # options_menu = tk.Menu(menubar, tearoff=0)
         data_menu = tk.Menu(menubar, tearoff=0)
-        developer_menu = tk.Menu(menubar, tearoff=0)
+        # developer_menu = tk.Menu(menubar, tearoff=0)
         about_menu = tk.Menu(menubar, tearoff=0)
         # Different options the user can change
         # make_option_menu(options_menu, master)
         make_file_option_menu(file_menu, master)
         make_data_menu(data_menu, master)
-        make_developer_menu(developer_menu, master)
+        # make_developer_menu(developer_menu, master)
         make_about_menu(about_menu, master)
 
         menubar.add_cascade(label="File", menu=file_menu)
         menubar.add_cascade(label="Data", menu=data_menu)
         # menubar.add_cascade(label="Options", menu=options_menu)
-        menubar.add_cascade(label="Developers", menu=developer_menu)
+        # menubar.add_cascade(label="Developers", menu=developer_menu)
         menubar.add_cascade(label="About", menu=about_menu)
 
         master.config(menu=menubar)
@@ -71,40 +71,40 @@ def make_file_option_menu(file_menu, master):
                           command=master.quit)
 
 
-def make_option_menu(options_menu, master):
-    """ Make command to add to the "Option" option in the menu
-    :param options_menu: tk.Menu to add the commands to
-    :param master: menu master
-    """
-    options_menu.add_cascade(label="Add custom TIA resistor",
-                             command=lambda: change_toplevel.EnterCustomTIAResistor(master))
+# def make_option_menu(options_menu, master):
+#     """ Make command to add to the "Option" option in the menu
+#     :param options_menu: tk.Menu to add the commands to
+#     :param master: menu master
+#     """
+#     options_menu.add_cascade(label="Add custom TIA resistor",
+#                              command=lambda: change_toplevel.EnterCustomTIAResistor(master))
 
-    user_set_label_options = tk.Menu(options_menu, tearoff=0)
-    options_menu.add_cascade(label="Set data label after a scan", menu=user_set_label_options)
-    user_set_label_options.add_cascade(label="True",
-                                       command=lambda: set_user_label_option(master, True))
-    user_set_label_options.add_cascade(label="False",
-                                       command=lambda: set_user_label_option(master, False))
+#     user_set_label_options = tk.Menu(options_menu, tearoff=0)
+#     options_menu.add_cascade(label="Set data label after a scan", menu=user_set_label_options)
+#     user_set_label_options.add_cascade(label="True",
+#                                        command=lambda: set_user_label_option(master, True))
+#     user_set_label_options.add_cascade(label="False",
+#                                        command=lambda: set_user_label_option(master, False))
 
-    electrode_config_options = tk.Menu(options_menu, tearoff=0)
-    options_menu.add_cascade(label="Set Electrode Configuration", menu=electrode_config_options)
-    electrode_config_options.add_cascade(label="Two electrode setting",
-                                         command=lambda: master.device.set_electrode_config(2))
-    electrode_config_options.add_cascade(label="Three electrode setting",
-                                         command=lambda: master.device.set_electrode_config(3))
-    user_set_voltage_source = tk.Menu(options_menu, tearoff=0)
-    options_menu.add_cascade(label="Set voltage source", menu=user_set_voltage_source)
-    user_set_voltage_source.add_cascade(label="VDAC (no external capacitor)",
-                                        command=lambda: set_voltage_source(master, "8-bit DAC"))
-    user_set_voltage_source.add_cascade(label="DVDAC (external capacitor added)",
-                                        command=lambda: set_voltage_source(master, "DVDAC"))
-    options_menu.add_cascade(label="Enter comments into logging file",
-                             command=lambda: change_toplevel.EnterLoggingInfo(master))
-    user_set_smoothing_value = tk.Menu(options_menu, tearoff=0)
-    for i in range(1, 6):
-        user_set_smoothing_value.add_cascade(label="{0} values".format(i),
-                                             command=lambda value=i: set_smoothing_value(master, value))
-    options_menu.add_cascade(label="Smooth data", menu=user_set_smoothing_value)
+#     electrode_config_options = tk.Menu(options_menu, tearoff=0)
+#     options_menu.add_cascade(label="Set Electrode Configuration", menu=electrode_config_options)
+#     electrode_config_options.add_cascade(label="Two electrode setting",
+#                                          command=lambda: master.device.set_electrode_config(2))
+#     electrode_config_options.add_cascade(label="Three electrode setting",
+#                                          command=lambda: master.device.set_electrode_config(3))
+#     user_set_voltage_source = tk.Menu(options_menu, tearoff=0)
+#     options_menu.add_cascade(label="Set voltage source", menu=user_set_voltage_source)
+#     user_set_voltage_source.add_cascade(label="VDAC (no external capacitor)",
+#                                         command=lambda: set_voltage_source(master, "8-bit DAC"))
+#     user_set_voltage_source.add_cascade(label="DVDAC (external capacitor added)",
+#                                         command=lambda: set_voltage_source(master, "DVDAC"))
+#     options_menu.add_cascade(label="Enter comments into logging file",
+#                              command=lambda: change_toplevel.EnterLoggingInfo(master))
+#     user_set_smoothing_value = tk.Menu(options_menu, tearoff=0)
+#     for i in range(1, 6):
+#         user_set_smoothing_value.add_cascade(label="{0} values".format(i),
+#                                              command=lambda value=i: set_smoothing_value(master, value))
+#     options_menu.add_cascade(label="Smooth data", menu=user_set_smoothing_value)
 
 
 def set_smoothing_value(master, value):
@@ -131,33 +131,33 @@ def set_user_label_option(master, value):
     master.graph.user_sets_labels_after_run = value
 
 
-def make_developer_menu(developer_menu, master):
-    """ Place to hold some custom commands to use when developing the device
-    :param developer_menu: root pull down menu
-    :param master:  root master
-    """
-    channel_option_menu = tk.Menu(developer_menu, tearoff=0)
-    developer_menu.add_cascade(label="Choose channel to export", menu=channel_option_menu)
-    for i in range(4):
-        channel_option_menu.add_cascade(label=str(i), command=lambda: master.set_adc_channel(i))
+# def make_developer_menu(developer_menu, master):
+#     """ Place to hold some custom commands to use when developing the device
+#     :param developer_menu: root pull down menu
+#     :param master:  root master
+#     """
+#     channel_option_menu = tk.Menu(developer_menu, tearoff=0)
+#     developer_menu.add_cascade(label="Choose channel to export", menu=channel_option_menu)
+#     for i in range(4):
+#         channel_option_menu.add_cascade(label=str(i), command=lambda: master.set_adc_channel(i))
 
-    data_option_menu = tk.Menu(developer_menu, tearoff=0)
-    developer_menu.add_cascade(label="Choose data export", menu=data_option_menu)
-    data_option_menu.add_cascade(label="Converted data",
-                                 command=lambda: master.set_data_type("Converted"))
-    data_option_menu.add_cascade(label="Raw adc data",
-                                 command=lambda: master.set_data_type("Raw Counts"))
+#     data_option_menu = tk.Menu(developer_menu, tearoff=0)
+#     developer_menu.add_cascade(label="Choose data export", menu=data_option_menu)
+#     data_option_menu.add_cascade(label="Converted data",
+#                                  command=lambda: master.set_data_type("Converted"))
+#     data_option_menu.add_cascade(label="Raw adc data",
+#                                  command=lambda: master.set_data_type("Raw Counts"))
 
-    developer_option_menu = tk.Menu(developer_menu, tearoff=0)
-    developer_menu.add_cascade(label="Get channel data", menu=developer_option_menu)
+#     developer_option_menu = tk.Menu(developer_menu, tearoff=0)
+#     developer_menu.add_cascade(label="Get channel data", menu=developer_option_menu)
 
-    for i in range(4):
-        developer_option_menu.add_cascade(label="channel " + str(i),
-                                          command=lambda channel=i: \
-                                              master.device.get_export_channel(channel))
+#     for i in range(4):
+#         developer_option_menu.add_cascade(label="channel " + str(i),
+#                                           command=lambda channel=i: \
+#                                               master.device.get_export_channel(channel))
 
-    developer_menu.add_cascade(label="Change timing PWM compare value",
-                               command=lambda: change_toplevel.ChangeCompareValue(master))
+#     developer_menu.add_cascade(label="Change timing PWM compare value",
+#                                command=lambda: change_toplevel.ChangeCompareValue(master))
 
 
 def make_about_menu(about_menu, master):
